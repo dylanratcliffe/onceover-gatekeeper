6c3836d39aa8373ebe987520c97d3e8c71cb2fd7
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2017-06-14 22:32:23 +1000

    Adding more config

--- lib/onceover/gatekeeper.rb
+++ lib/onceover/gatekeeper.rb
@@ -14,13 +14,17 @@ class Onceover
     def self.pre_write_spec_test(tst)
       c = Onceover::Gatekeeper::Compiler.new
 
-      c.node_name = 'foo'
+      c.node_name = 'foo' # TODO: Fix
       c.facts = tst.nodes[0].fact_set
-      #c.hiera_config = @repo
+      c.hiera_config = @repo.hiera_config
+      c.code = "include #{tst.classes[0].name}"
+      c.environment = 'production' # TODO: Fix
+      c.modulepath = @repo.temp_modulepath
 
       require 'pry'
       binding.pry
 
+      #c.hiera_config = @repo
     end
   end
 end
