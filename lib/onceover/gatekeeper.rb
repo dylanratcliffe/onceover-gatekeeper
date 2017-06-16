1f298f3287e6e364c9885c2cbb3852c14202a886
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2017-06-16 18:28:38 +1000

    Added template functionality

--- lib/onceover/gatekeeper.rb
+++ lib/onceover/gatekeeper.rb
@@ -7,24 +7,40 @@ class Onceover
     attr_accessor :config, :repo
 
     def self.pre_prepare(runner)
+      # Get the config and repo objects that are in use for reference later
       @config = runner.config
       @repo   = runner.repo
     end
 
     def self.pre_write_spec_test(tst)
+      require 'json'
+
       c = Onceover::Gatekeeper::Compiler.new
 
-      c.node_name = 'foo' # TODO: Fix
+      # Set up settings from onceover
+      c.node_name = tst.nodes[0].fact_set['fqdn']
       c.facts = tst.nodes[0].fact_set
       c.hiera_config = @repo.hiera_config
       c.code = "include #{tst.classes[0].name}"
-      c.environment = 'production' # TODO: Fix
+      c.environment = 'production'
       c.modulepath = @repo.temp_modulepath.split(':')
+
+      # Template variables
+      examples_name = tst.classes[0].name
+      class_name    = tst.classes[0].name
+      resources = c.build
+
 
       require 'pry'
       binding.pry
 
       #c.hiera_config = @repo
+    end
+
+    def evaluate_template(template_name,bind)
+      template_dir = File.expand_path('../../templates',File.dirname(__FILE__))
+      template = File.read(File.expand_path("./#{template_name}",template_dir))
+      ERB.new(template, nil, '-').result(bind)
     end
   end
 end
